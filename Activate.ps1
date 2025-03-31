c21654583d84360b32426c59133b82eb2986e1e4
unknown <andrew.e.cumming@gmail.com>
2025-03-30 20:58:07 -0400

    Refactored activate script to not use invoke-expression.

--- Activate.ps1
+++ Activate.ps1
@@ -12,14 +12,16 @@ function Invoke-ActivateScript {
     $EnvFolder = "$CurrentFolder-venv"
     Write-Host "Using venv folder of [$EnvFolder]"
 
+    $ActivateScriptPath = Join-Path ($PSScriptRoot | Split-Path -Parent) "$EnvFolder/Scripts/Activate.ps1"
+
     if (Test-Path $EnvFolder -PathType Container) {
         Write-Output "Virtual environment already exists. Activating virtual environment..."
-        Invoke-Expression "./$EnvFolder/Scripts/Activate.ps1"
+        & $ActivateScriptPath
     } else {
         Write-Output "Virtual environment not found. Creating virtual environment and installing dependencies..."
 
         python -m venv $EnvFolder `
-            && Invoke-Expression "./$EnvFolder/Scripts/Activate.ps1" `
+            && & $ActivatePath `
             && pip install -r requirements.txt
     }
 }
